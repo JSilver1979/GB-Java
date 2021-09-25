d6eb8a3b0ee88543ee0b5eaca3e59eaf5f189d23
JSilver1979 <glarosh@gmail.com>
2021-09-25 16:32:55 +0300

    Lesson1 Homework

---
+++ src/main/java/ru/GeekBrains/Lesson1/HomeWorkApp.java
@@ -0,0 +1,44 @@
+package ru.GeekBrains.Lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 7;
+        int b = -9;
+        int result = a + b;
+
+        if (result >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 99;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 4;
+
+        System.out.println(a >= b ? "a >= b" : "a < b");
+    }
+}
